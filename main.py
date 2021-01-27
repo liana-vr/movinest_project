d293bcac6aa5cf617d5015dae2c5574851f7cbd7
lianatime <liana.vanrensburg@gmail.com>
2021-01-27 19:02:55 +1300

    Submit Button Fixed

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ TV_IMG_URL = "https://image.tmdb.org/t/p/w500"
 app = Flask(__name__)
 Bootstrap(app)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
