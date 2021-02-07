7fa22351e2cd69dd28e9c63604d72b8a94d32366
lianatime <liana.vanrensburg@gmail.com>
2021-02-08 11:05:14 +1300

    Postgres added

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ TV_IMG_URL = "https://image.tmdb.org/t/p/w500"
 
 app = Flask(__name__)
 Bootstrap(app)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movinest.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
