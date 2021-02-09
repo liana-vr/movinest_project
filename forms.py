5f27292645cd05a5469a53fa99342ff741403416
lianatime <liana.vanrensburg@gmail.com>
2021-02-09 17:09:34 +1300

    css min added

--- forms.py
+++ forms.py
@@ -44,7 +44,7 @@ class FindMovieForm(FlaskForm):
 
 
 class RateMovieForm(FlaskForm):
-    rating = StringField("Your Rating Out of 10 e.g. 7.5", [validators.DataRequired()])
+    rating = StringField("Rating Out of 10 e.g. 7.5", [validators.DataRequired()])
     review = StringField("Write A Short Review")
     submit = SubmitField("Done")
 
